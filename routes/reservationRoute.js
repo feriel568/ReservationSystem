6d87ec352974d82673ab879c2b96aebff5893843
ferielRabah <ferielrabah1@gmail.com>
2024-03-25 05:06:53 +0100

    Adding reservation routes

---
+++ routes/reservationRoute.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const reservationController = require('../controllers/reservationontroller')
+const auth = require('../middleware/auth')
+
+
+router.post('/add' , auth ,reservationController.makeReservation)
+router.delete('/delete/:id' , auth ,reservationController.cancelReservation)
+
+router.get('/myReservation/:userId' , auth ,reservationController.getUserReservations)
+
+router.get('/all', auth ,reservationController.getAllReservations)
+
+module.exports = router;
